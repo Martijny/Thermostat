455ac781162b6f0bc34ac8933bdcc8f73a1327bc
Martijn Copier <Martijn_copier_4@hotmail.com>
2016-06-06 23:03:29 +0200

    Allemaal nieuwe zooi

--- app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
+++ app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
@@ -1,19 +1,18 @@
 package nl.tue.demothermostat;
 
+import nl.tue.demothermostat.circularseekbar.CircularSeekBar.OnCircularSeekBarChangeListener;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.SeekBar;
 import android.widget.TextView;
-
-import java.text.DecimalFormat;
+import nl.tue.demothermostat.circularseekbar.CircularSeekBar;
 
 public class ThermostatActivity extends Activity {
 
-    double vtemp = 21;
+    double vtemp = 5;
     TextView temp;
     TextView setTemp;
     TextView dDayTemp;
@@ -32,6 +31,8 @@ public class ThermostatActivity extends Activity {
         setTemp = (TextView)findViewById(R.id.setTemp);
         dDayTemp = (TextView)findViewById(R.id.dDayTemp);
         dNightTemp = (TextView)findViewById(R.id.dNightTemp);
+        final CircularSeekBar seekbar = (CircularSeekBar) findViewById(R.id.circularSeekBar1);
+        seekbar.setMax(250);
 
         ImageView bPlus = (ImageView)findViewById(R.id.bPlus);
         bPlus.setImageResource(R.drawable.add_button);
@@ -61,15 +62,17 @@ public class ThermostatActivity extends Activity {
         bPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                vtemp += 0.1;
+                vtemp = (Double.parseDouble(temp.getText().toString())+0.1);
                 temp.setText(String.format("%.1f", vtemp));
+                seekbar.setProgress((int) (vtemp * 10));
             }
         });
         bMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                vtemp -= 0.1;
+                vtemp = (Double.parseDouble(temp.getText().toString())-0.1);
                 temp.setText(String.format( "%.1f", vtemp ));
+                seekbar.setProgress((int)(vtemp*10));
             }
         });
         bleft.setOnClickListener(new View.OnClickListener() {
@@ -108,6 +111,25 @@ public class ThermostatActivity extends Activity {
                 }
             }
         });
+        seekbar.setOnSeekBarChangeListener(new CircleSeekBarListener());
 
+    }
+
+    public class CircleSeekBarListener implements CircularSeekBar.OnCircularSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(CircularSeekBar circularSeekBar, int progress, boolean fromUser) {
+            vtemp = (progress*0.1);
+            temp.setText(String.format( "%.1f", vtemp ));
+        }
+
+        @Override
+        public void onStopTrackingTouch(CircularSeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(CircularSeekBar seekBar) {
+
+        }
     }
 }
---
+++ app/src/main/java/nl/tue/demothermostat/circularseekbar/CircleSeekBarListener.java
@@ -0,0 +1,5 @@
+package nl.tue.demothermostat.circularseekbar;
+
+/**
+ * Created by martijn on 06/06/2016.
+ */
