129195fb579ab5d27b30d9b1288506b50a52f05e
Galić <p.galic@student.tue.nl>
2016-06-10 17:23:40 +0200

    Changes to the Weekly thing Thanks to the master LLOEN

---
+++ app/src/main/java/org/thermostatapp/util/Week1.java
@@ -0,0 +1,15 @@
+package org.thermostatapp.util;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import nl.tue.demothermostat.R;
+
+public class Week1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week1);
+    }
+}
