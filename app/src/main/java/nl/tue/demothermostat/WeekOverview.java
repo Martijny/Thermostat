072eb464f86925399f486591a4ae3fd532503c99
Martijn Copier <martijn_copier_4@hotmail.com>
2016-06-16 14:25:01 +0200

    New

--- app/src/main/java/nl/tue/demothermostat/WeekOverview.java
+++ app/src/main/java/nl/tue/demothermostat/WeekOverview.java
@@ -45,6 +45,8 @@ public class WeekOverview extends Activity {
         buttonArray[5] = saturdayB;
         buttonArray[6] = sundayB;
 
+        this.getActionBar().hide();
+
 
         mondayB.setOnClickListener(new View.OnClickListener() {
             @Override
